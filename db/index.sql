c4701cc85c49814785f558c163f7bc45ba194965
woderchen <woderchen@163.com>
2015-07-20 03:56:33 +0800

    updated

--- db/index.sql
+++ db/index.sql
@@ -6,3 +6,5 @@ ADD INDEX `ix_tb_parking_prod_type` (`type` ASC);
 
 ALTER TABLE `tb_parking_prod_loc` 
 ADD INDEX `ix_tb_parking_prod_open` (`is_open` ASC);
+
+ALTER TABLE `tb_allowance_offer` ADD INDEX `ix_allowance_offer_parkid` (`park_id` ASC);
