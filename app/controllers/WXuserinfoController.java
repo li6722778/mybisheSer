5ca22c0c0664dc04936f49b8e4e73f37d52ad1a3
meixuesheng <308348194@qq.com>
2015-09-11 17:20:21 +0800

    mxs

---
+++ app/controllers/WXuserinfoController.java
@@ -0,0 +1,134 @@
+package controllers;
+
+import java.util.Date;
+import java.util.Random;
+
+import models.info.TOptions;
+import models.info.TuserInfo;
+import models.info.Twxuserinfo;
+import action.BasicAuth;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import play.Logger;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import utils.ComResponse;
+
+/**
+ * @author mxs E-mail:308348194@qq.com
+ * @version 创建时间：2015年9月10日 下午2:02:40
+ */
+public class WXuserinfoController extends Controller {
+	
+	public static Gson gsonBuilderWithExpose = new GsonBuilder()
+	.excludeFieldsWithoutExposeAnnotation()
+	.setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+	
+	
+
+	
+	/**
+	 * 根据url，得到用户列表
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static Result getuserinfolist(String url) {
+		Logger.info("start to get userinfolist");
+		String json = gsonBuilderWithExpose.toJson(Twxuserinfo.getwxuserinfos(url));
+		JsonNode jsonNode = Json.parse(json);
+		Logger.debug("got Data:" + json);
+		return ok(jsonNode);
+	}
+	
+	
+	public static Result saveuserinfo ( String openid, String nickname, String headimgurl,int getcoupnprice, String uniqueurl, String userphone)
+	{
+		
+		Logger.info("start to save Wxshare userinfo");
+		ComResponse<String> response = new ComResponse<String>();
+	
+		Twxuserinfo twxuserinfo =Twxuserinfo.getuser(uniqueurl, openid);
+		//存在数据，更新手机号码
+		if(twxuserinfo!=null)
+		{
+			Twxuserinfo.updateuserinfo(uniqueurl, userphone, openid);
+			response.setResponseStatus(ComResponse.STATUS_OK);
+			LogController.info("update wxshare  userinfo success:");
+		}
+		else {
+			try {
+				String shareword = getRandomshareword();
+				if(shareword!=null&&(!shareword.equals(""))){
+					Twxuserinfo.saveuserinfo(openid, nickname, headimgurl,shareword, getcoupnprice, uniqueurl, userphone);
+					response.setResponseStatus(ComResponse.STATUS_OK);
+					LogController.info("save wxshare  userinfo success:");	
+				}
+				
+				
+			} catch (Exception e) {
+				response.setResponseStatus(ComResponse.STATUS_FAIL);
+			}
+			
+		}
+		
+		
+		String tempJsonString = gsonBuilderWithExpose.toJson(response);
+		return ok("success_jsonpCallback(" + tempJsonString + ")");
+	}
+	
+	
+	public static Result updateuserinfo(String userphone,String url,String openid) {
+		Logger.info("change phonenumber");
+		ComResponse<String> response = new ComResponse<String>();
+		try {
+			Twxuserinfo.updateuserinfo(url, userphone, openid);
+			response.setResponseStatus(ComResponse.STATUS_OK);
+			LogController.info("change phonenumber");
+		} catch (Exception e) {
+			response.setResponseStatus(ComResponse.STATUS_FAIL);
+		}
+		
+		String tempJsonString = gsonBuilderWithExpose.toJson(response);
+		return ok("success_jsonpCallback(" + tempJsonString + ")");
+	}
+	
+	
+	public static String getRandomshareword()
+	{
+		// 获取设置的领取优惠劵分享语
+	  TOptions option = TOptions.findOption(10);
+	  String[] sharewords = null;
+	  String shareword = option.textObject;
+		if (shareword != null && !(shareword.toString().trim().equals(""))) {
+			shareword = shareword.replace("，", ",");
+            sharewords = shareword.split(",");
+		}
+		if(sharewords!=null){
+	   int max=sharewords.length-1;
+       int min=1;
+       Random random = new Random();
+       int s = random.nextInt(max)%(max-min+1) + min;
+       Logger.info("random s:"+s);
+       return sharewords[s-1];
+
+	 }
+
+		return null;
+		
+	}
+	
+	public static Result getcurrentuserinfo(String openid) {
+		Logger.info("start to get userinfolist");
+		String json = gsonBuilderWithExpose.toJson(Twxuserinfo.getwxuserinfo(openid));
+		JsonNode jsonNode = Json.parse(json);
+		Logger.debug("got Data:" + json);
+		return ok(jsonNode);
+	}
+	
+
+}
---
+++ app/models/info/Twxuserinfo.java
@@ -0,0 +1,159 @@
+package models.info;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import play.data.format.Formats;
+import play.db.ebean.Model;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.TxRunnable;
+import com.google.gson.annotations.Expose;
+
+/**
+ * @author mxs E-mail:308348194@qq.com
+ * @version 创建时间：2015年9月9日 下午5:25:18
+ */
+
+@Entity
+@Table(name = "tb_wxuser")
+public class Twxuserinfo extends Model{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Expose
+	@Column(length = 20)
+	public String openid;
+	
+	@Column(length = 20)
+	@Expose
+	public String nickname;
+	
+	@Column(length = 50)
+	@Expose
+	public String headimgurl;
+	
+	
+	@Formats.DateTime(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(columnDefinition = "timestamp NULL")
+	@Expose
+	public Date sharetDate;
+	
+	
+	@Column(length = 50)
+	@Expose
+	public String shareword;
+	
+	@Expose
+	public int getcoupnprice;
+	
+	
+	@Column(length = 50)
+	@Expose
+	public String uniqueurl;
+	
+	
+	
+	@Column(length = 30)
+	@Expose
+	public String userphone;
+	
+	
+	// 查询finder，用于其他方法中需要查询的场景
+	public static Finder<String, Twxuserinfo> find = new Finder<String, Twxuserinfo>(
+			String.class, Twxuserinfo.class);
+	
+	
+	
+	public static void saveuserinfo(final String openid,final String nickname,final String headimgurl,
+			final String shareword, final int getcoupnprice,final String uniqueurl,final String userphone) {
+
+		Ebean.execute(new TxRunnable() {
+			public void run() {
+				Twxuserinfo userinfo= new Twxuserinfo();
+				userinfo.openid=openid;
+				userinfo.nickname =nickname;
+				userinfo.headimgurl=headimgurl;
+			    userinfo.sharetDate=new Date();
+			    userinfo.shareword=shareword;
+			    userinfo.getcoupnprice=getcoupnprice;
+			    userinfo.uniqueurl=uniqueurl;
+			    userinfo.userphone=userphone;
+				Ebean.save(userinfo);
+
+			}
+
+		});
+
+	}
+	
+	
+	
+	public static void updateuserinfo(final String url,final String phonenumber,final String openid) {
+
+		Ebean.execute(new TxRunnable() {
+			public void run() {
+				Twxuserinfo userinfo= find.where().eq("uniqueurl", url).eq("openid", openid).findUnique();
+				userinfo.userphone=phonenumber;
+				Ebean.update(userinfo);
+
+			}
+
+		});
+
+	}
+
+
+
+	/**
+	 * 得到该分享url获得优惠劵用户列表
+	 * 
+	 * @param parkId
+	 * @return
+	 */
+	public static List<Twxuserinfo> getwxuserinfos(String url) {
+		List<Twxuserinfo> result = find.where().eq("uniqueurl", url).ne("getcoupnprice", null)
+				.findList();
+		return result;
+	}
+	
+/**
+ * 
+ */
+	public static Twxuserinfo getuser(String url,String openid)
+	{
+		Twxuserinfo wxuserinfo = find.where().eq("uniqueurl", url).eq("openid", openid).findUnique();
+		if(wxuserinfo!=null)
+		{
+			return wxuserinfo;
+		}
+		
+		return null;
+		
+	}
+	
+	/**
+	 * 根据用户唯一标识openid获取用户信息
+	 * 
+	 * @param openid
+	 * @return
+	 */
+	public static Twxuserinfo getwxuserinfo(String openid) {
+		List<Twxuserinfo> result = find.where().eq("openid", openid).findList();
+		if(result!=null)
+		{	Twxuserinfo wxuserinfo=result.get(0);
+		return wxuserinfo;
+		}
+		return null;
+	}
+	
+	
+}
